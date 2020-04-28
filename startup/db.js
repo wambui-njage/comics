e90faa2f9e3b4e2af273b1ab98c96896b967fc4f
Wambui Njage <wambui.njage@emomentum.com>
2020-04-28 08:49:04 +0300

    Update db.js

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,8 @@
 const mongoose=require('mongoose');
+const config = require("config");
 module.exports=function(){
 
-    mongoose.connect("mongodb+srv://comic:LCuQzVLDAUCzG7Nx@cluster0-e1gwz.azure.mongodb.net/test?retryWrites=true&w=majority",{  useCreateIndex: true,
+    mongoose.connect(config.get("db_url"),{  useCreateIndex: true,
     useNewUrlParser: true })
     .then((result)=>console.log("we in this mug"))
     .catch(err=>console.log("the devil is working",err));
